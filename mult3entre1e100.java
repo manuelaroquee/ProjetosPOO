df3ddf2c98f23f4436a4a75e6a3d296b895fa6e4
Manuela Roque <65978895+manuelaroquee@users.noreply.github.com>
2024-03-28 14:22:28 -0400

    Create mult3entre1e100.java

---
+++ mult3entre1e100.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[ ] args) {
+  System.out.println("Múltiplos de 3 entre 1 e 100:");
+
+  for (int i = 1; i <= 100; i++) {
+    if (i % 3 == 0) {
+    System.out.println(i);
+      }
+    }
+  }
+}
